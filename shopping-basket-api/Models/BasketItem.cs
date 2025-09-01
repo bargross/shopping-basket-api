2fa34e2fe74637ad09dd53fb32946fcacd10f8a2
Leo Mengesha <barggros@gmail.com>
2025-09-01 22:04:57 +0100

    removed useless comment

--- shopping-basket-api/Models/BasketItem.cs
+++ shopping-basket-api/Models/BasketItem.cs
@@ -8,6 +8,6 @@ namespace shopping_basket_api.Models
         public float Price { get; set; }
 
         [MinLength(1), MaxLength(100)]
-        public float? Discount { get; set; } // in %
+        public float? Discount { get; set; }
     }
 }
